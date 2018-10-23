c22e6fac1e02e3a0812c002446ec44f91c8910b8
Denis Antonov <antonov@appvelox.ru>
2018-10-23 20:16:16 +0300

    add new models

---
+++ alicemsg/models.py
@@ -0,0 +1,44 @@
+class CallbackRequest(object):
+    def __init__(self, session: dict, locale: str, timezone: str, payload: str):
+        if not isinstance(session, dict):
+            raise TypeError('CallbackRequest.session must be an instance of dict')
+        if not isinstance(locale, str):
+            raise TypeError('CallbackRequest.locale must be an instance of str')
+        if not isinstance(timezone, str):
+            raise TypeError('CallbackRequest.timezone must be an instance of str')
+        if not isinstance(payload, str):
+            raise TypeError('CallbackRequest.payload must be an instance of str')
+        self.session = session
+        self.locale = locale
+        self.timezone = timezone
+        self.payload = payload
+
+
+class TextMessageRequest(object):
+    def __init__(self, session: dict, locale: str, timezone: str, text: str, tokens: list):
+        if not isinstance(session, dict):
+            raise TypeError('TextMessageRequest.session must be an instance of dict')
+        if not isinstance(locale, str):
+            raise TypeError('TextMessageRequest.locale must be an instance of str')
+        if not isinstance(timezone, str):
+            raise TypeError('TextMessageRequest.timezone must be an instance of str')
+        if not isinstance(text, str):
+            raise TypeError('TextMessageRequest.text must be an instance of str')
+        if not isinstance(tokens, list):
+            raise TypeError('TextMessageRequest.tokens must be an instance of list')
+        self.session = session
+        self.locale = locale
+        self.timezone = timezone
+        self.text = text
+        self.tokens = tokens
+
+
+class Message(object):
+    def __init__(self, text: str, buttons: list = None):
+        if not isinstance(text, str):
+            raise TypeError('Message.text must be an instance of str')
+        self.text = text
+        if buttons:
+            if not isinstance(buttons, list):
+                raise TypeError('Message.buttons must be an instance of list')
+            self.buttons = buttons
